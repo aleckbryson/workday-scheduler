473d34ef379d14330da6365eadc75b8e77eb1a63
Aleck Tuluenga <aleckbryson@gmail.com>
2020-08-11 16:26:07 -0400

    Momemnt.js updated to display day, date, and time; timeslot array slightly modified

---
+++ Assets/script.js
@@ -0,0 +1,17 @@
+//Day, date and time added for the current info using moment.js
+$('#currentDate').text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+//Array made for the different time slots
+var timeSlots = {
+"9AM": "", "10AM": "", "11AM" : "", "12PM": "", "1PM" : "", "2PM" : "", "3PM" : "", "4PM" : "", "5PM" : ""
+};
+
+
+var timeDiv = $(".time-block");
+var description = $(".description")
+
+$.each(timeSlots, function(i, time) {
+timeDiv.append("<div>" + time + "</div>");
+});
+
+
