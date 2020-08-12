524051811cccd9cf1be77971e7ee9ab70ae3b4bd
Aleck Tuluenga <aleckbryson@gmail.com>
2020-08-11 22:06:45 -0400

    funstions added for loading, calling, and saving the tasks

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,81 @@
 //Day, date and time added for the current info using moment.js
-$('#currentDate').text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, h:mm:ss a'));
+var currentDate = $('#currentDate').text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, h:mm:ss a'));
 
 //Array made for the different time slots
 var timeSlots = {
 "9AM": "", "10AM": "", "11AM" : "", "12PM": "", "1PM" : "", "2PM" : "", "3PM" : "", "4PM" : "", "5PM" : ""
 };
 
-
 var timeDiv = $(".time-block");
 
+function hourNumber(houText) {
+    switch(houText) {
+      case "8 AM": 
+        return 8;
+      case "9 AM": 
+        return 9;
+      case "10 AM": 
+        return 10;
+      case "11 AM": 
+        return 11;
+      case "12 PM": 
+        return 12;
+      case "1 PM": 
+        return 13;
+      case "2 PM": 
+        return 14;
+      case "3 PM": 
+        return 15;
+      case "4 PM": 
+        return 16;
+      case "5 PM": 
+        return 17;
+    }
+  }
 
+  $(document).ready(function(){
+    if(!localStorage.getItem(timeSlots)) {
+      modifyTask(timeSlots);
+    } else {
+      modifyTask(JSON.parse(localStorage.getItem(timeSlots)));
+    }
+  })
 
+  $("button").click(function() {
+    value = $(this).siblings("textarea").val();
+    houText = $(this).siblings("div").text();
+    
+    taskSave(houText, value);
+  });
+  
+  function loadLocal() {
+    result = localStorage.getItem(timeSlots)
+    return (result ? result : timeSlots);
+  }
+  
+  function callLocal() {
+    localStorage.setItem(timeSlots, JSON.stringify(timeSlots));
+  };
+  
+  function saveLocal(object) {
+    localStorage.setItem(timeSlots, JSON.stringify(object));
+  }
+  
+  function taskSave(houText, val) {
+    if(!localStorage.getItem(timeSlots)) {
+      callLocal();
+    }
+  
+    var loggedHours = JSON.parse(localStorage.getItem(timeSlots));
+    loggedHours[houText] = val
+  
+    saveLocal(loggedHours);
+  }
+  
+  function modifyTask(hourTask) {
+    $(".time-block").each(function(block) {
+      let res = $(this).children("div");
+      $(this).children("textarea").text(hourTask[res.text()]);
+    })
+  }
+  
